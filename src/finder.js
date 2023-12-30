d8d9248f8f306e90da9d85c661e062b6156f6a82
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-30 14:52:49 -0300

    at

--- src/finder.js
+++ src/finder.js
@@ -9,5 +9,18 @@
  * @return {SantosDummontAspectRatio}
  * */
 function convert_aspect_ratio(width,height){
-    
+    //convert 1920x1080 to 16x9
+    let aspect_ratio = width/height;
+    let width_ratio = 1;
+    let height_ratio = 1;
+    if(aspect_ratio > 1){
+        width_ratio = aspect_ratio;
+    }
+    if(aspect_ratio < 1){
+        height_ratio = 1/aspect_ratio;
+    }
+    return {
+        width:width_ratio,
+        height:height_ratio
+    };
 }
