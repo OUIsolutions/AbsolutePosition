0e793ee8adb8d19226435269e29c346c8cb2789e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-30 15:31:36 -0300

    at

--- src/finder.js
+++ src/finder.js
@@ -2,10 +2,12 @@
  * @typedef {object} SantosDummontAspectRatio
  * @property {number} width
  * @param {number} height
+ * @param {boolean} is_horizontal
+ * @param {boolean} is_vertical
  * */
 /**
  * @param {number} width
- * @param {height} height
+ * @param {number} height
  * @return {SantosDummontAspectRatio}
  * */
 function SantosDummont_convert_aspect_ratio(width,height){
@@ -20,6 +22,8 @@ function SantosDummont_convert_aspect_ratio(width,height){
         height_ratio = 1/aspect_ratio;
     }
     return {
+        is_horizontal:aspect_ratio >= 1,
+        is_vertical:aspect_ratio < 1,
         width:width_ratio,
         height:height_ratio
     };
@@ -34,22 +38,13 @@ function SantosDummont_convert_aspect_ratio(width,height){
 function SantosDummont_find_closest_measure(measures, browser_width, browser_height) {
     let closest = measures[0];
     let browser_ratio = SantosDummont_convert_aspect_ratio(browser_width, browser_height);
-    
-    let browser_horizontal = browser_ratio.width > browser_ratio.height;
-    let vertical_browser = browser_ratio.width < browser_ratio.height;
-
-    for (let measure of measures) {
-        let measure_ratio = SantosDummont_convert_aspect_ratio(measure.dimensions.width, measure.dimensions.height);
-        let horizontal_measure = measure_ratio.width > measure_ratio.height;
-        let vertical_measure = measure_ratio.width < measure_ratio.height;
-
-        if (browser_horizontal && horizontal_measure) {
+    for(let measure of measures){
+        let measure_ratio = SantosDummont_convert_aspect_ratio(measure.horizontal_ratio, measure.vertical_ratio);
+        if(browser_ratio.is_horizontal === measure_ratio.is_horizontal){
             closest = measure;
         }
-        if (vertical_browser && vertical_measure) {
-            closest = measure;
-        }
+
+
     }
-    
     return closest;
 }
