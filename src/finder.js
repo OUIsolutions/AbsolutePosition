2b8bf6306b500ec68ebebeea455467fc2045f6f2
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-30 14:51:53 -0300

    at

--- src/finder.js
+++ src/finder.js
@@ -1,2 +1,13 @@
-
-
+/**
+ * @typedef {object} SantosDummontAspectRatio
+ * @property {number} width
+ * @param {number} height
+ * */
+/**
+ * @param {number} width
+ * @param {height} height
+ * @return {SantosDummontAspectRatio}
+ * */
+function convert_aspect_ratio(width,height){
+    
+}
