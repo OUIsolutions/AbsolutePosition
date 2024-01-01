1676669df8ddab7f8886392d7798d81b53c37da3
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-01 14:33:06 -0300

    at

--- build.py
+++ build.py
@@ -13,14 +13,15 @@ for e in elements:
 with open('AbsolutePosition.js', 'w') as f:
     f.write(output)
 
-exemples = listdir('exemples')
+exemples = listdir('internal/exemples')
 with open('internal/readme.md', 'r') as f:
     readme_code = f.read()
 
 for e in exemples:
-    with open(f'exemples/{e}', 'r') as f:
+    with open(f'internal/exemples/{e}', 'r') as f:
         output = f.read()
         readme_code = readme_code.replace(f"#ref:{e}", f'```html\n{output}\n```')
+
 
 with open('readme.md', 'w') as f:
     f.write(readme_code)
