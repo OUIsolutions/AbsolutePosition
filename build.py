cae716d98bc45827edd509b277b3ece182bea443
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-01 21:01:28 -0300

    att

--- build.py
+++ build.py
@@ -1,4 +1,6 @@
 from os import listdir
+import hashlib
+
 elements = [
     'src/constants.js',
      'src/error.js',
@@ -28,3 +30,19 @@ if "#ref" in readme_code:
     raise Exception(f"Missing reference {readme_code.split('#ref')[1]}")
 with open('readme.md', 'w') as f:
     f.write(readme_code)
+
+#generate the sha for output 
+sha = hashlib.sha256()
+
+#replacing html links 
+link = 'https://cdn.jsdelivr.net/gh/OUIsolutions/AbsolutePosition@latest/AbsolutePosition.js'
+div = f'src="{link}" integrity="{sha.hexdigest()}"'
+
+
+for e in listdir('exemples_not_linked'):
+    with open(f'exemples_not_linked/{e}', 'r') as f:
+        output = f.read()
+        output = output.replace('#lib#', div)
+        with open(f'exemples/{e}', 'w') as f:
+            f.write(output)
+   
